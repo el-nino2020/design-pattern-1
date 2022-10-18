8a32d037e0daed86b3aa5fc66ff01bf2184c253c
El_nino <null>
2022-10-18 10:57:54 +0800

    template method模式

---
+++ src/template_method/AbstractDisplay.java
@@ -0,0 +1,20 @@
+package template_method;
+
+public abstract class AbstractDisplay {
+    public abstract void open();
+
+    public abstract void print();
+
+    public abstract void close();
+
+    //final确保处理流程不会被子类更改
+    public final void display() {
+        open();
+
+        for (int i = 0; i < 5; i++) {
+            print();
+        }
+
+        close();
+    }
+}
