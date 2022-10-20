de3209333580fe44742d766e04f5adf70ff001fd
El_nino <null>
2022-10-20 09:22:25 +0800

    update comment

--- src/common/Param.java
+++ src/common/Param.java
@@ -8,7 +8,7 @@ public class Param {
     }
 
     /**
-     * 本项目的src目录在当前主机的绝对路径
+     * 本项目的src目录在当前主机的绝对路径，具体值视主机而定
      */
     public static final String SRC_PATH = "D:\\CODES(daima)\\design-pattern-1\\src\\";
 }
